8137498451247f924147bf6460fa36f8497a20d1
Brendow271 <pashashkuropat@yandex.ru>
2024-07-03 18:42:05 +0300

    Initializing and configuring
    - Add vuetify
    - Add scss
    - Add pinia

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
-import './assets/main.css'
+// import './assets/main.css'
+import 'vuetify/styles'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import pinia from './stores'
+import './styles/main.scss'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(vuetify)
+app.use(pinia) // Используйте Pinia
+
+app.mount('#app')
