274b9e780157346101c96b0db67f5db937c211e4
Brendow271 <pashashkuropat@yandex.ru>
2024-07-17 18:38:06 +0300

    Created Popup component

---
+++ src/components/Popup.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container fluid>
+    <p>Selected Button: {{ selectedPayer }}</p>
+    <v-radio-group v-model="selectedPayer">
+      <v-radio
+          v-for="(friend, index) in friends"
+          :key="index"
+          :label="friend.name"
+          :value="friend.name"
+      ></v-radio>
+    </v-radio-group>
+  </v-container>
+</template>
+
+<script>
+import {useFriendsStore} from '../stores/friends.js';
+export default {
+  setup () {
+    const friendsStore = useFriendsStore();
+    return {
+      friends: friendsStore.friends,
+      selectedPayer: 'Плательщик',
+    }
+  },
+}
+</script>
