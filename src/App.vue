5ab3a4fe24b53d84ff22130e90e80c340989e6c0
Brendow271 <pashashkuropat@yandex.ru>
2025-02-25 23:31:38 +0300

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <v-app>
     <v-main>
-      <router-view>
-      </router-view>
+
     </v-main>
-  </v-app>
+  </v-app>  <router-view>
+</router-view>
 </template>
 
 <script>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home d-flex justify-center align-center flex-column">
     <v-img src="../../public/images/logo.svg" alt="Logo" class="logo"/>
+
     <h1 class="text-white text-uppercase" id="logoName">
       Bill Splitter
     </h1>
